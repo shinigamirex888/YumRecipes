ba1b14a8aac90142c257fffeb95a35a3442de064
Snehil <snehilsneh888@gmail.com>
2021-09-11 11:05:27 +0530

    Added Categories Screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 
-class CategoriesScreen extends StatelessWidget {
-  
+import './dummy_data.dart';
+import './category_item.dart';
 
+class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      maxCrossAxisExtent: 200,
-      childAspectRatio: 3/2,
-      crossAxisSpacing: 20, 
-      mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('YumRecipes'),
       ),
-        children: <Widget>[
-          
-        ],
-        );
+      body:GridView(
+        padding: const EdgeInsets.all(25),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(catData.title, catData.color))
+          .toList(),
+    ) ,
+      );
   }
 }
