dd31ce5be224495e878e07585be70d0c7650c0af
Snehil <snehilsneh888@gmail.com>
2021-09-12 10:14:27 +0530

    Navigation b/w screens

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Category Meals'),
+      ),
+      body:(
+      Center(
+      child: Text(
+        'CategoryMealsScreen'),
+        )
+      ),
+      );
+  }
+}
