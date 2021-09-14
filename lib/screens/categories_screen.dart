abd0ea063a6964397ba0ac4e285d2d6f41356798
Snehil <snehilsneh888@gmail.com>
2021-09-14 21:02:34 +0530

    Added bottom tabBar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,12 +6,8 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('YumRecipes'),
-      ),
-      body:GridView(
-        padding: const EdgeInsets.all(25),
+    return GridView(
+      padding: const EdgeInsets.all(25),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
@@ -19,9 +15,9 @@ class CategoriesScreen extends StatelessWidget {
         mainAxisSpacing: 20,
       ),
       children: DUMMY_CATEGORIES
-          .map((catData) => CategoryItem(catData.id, catData.title, catData.color))
+          .map((catData) =>
+              CategoryItem(catData.id, catData.title, catData.color))
           .toList(),
-    ) ,
-      );
+    );
   }
 }
